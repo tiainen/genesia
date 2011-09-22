e9c713e75d6d68813fc76a6adb41ac6822e75e53
Joeri Sykora <joeri@sertik.net>
2011-09-22 21:57:39 +0200

    center controls

--- genesia/src/main/java/net/sertik/genesia/screen/GameSelection.java
+++ genesia/src/main/java/net/sertik/genesia/screen/GameSelection.java
@@ -12,8 +12,9 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
@@ -35,8 +36,6 @@ public class GameSelection extends StackPane {
   private final ToggleGroup gameLevel;
 
   public GameSelection(final Genesia genesia) {
-    setAlignment(Pos.CENTER_LEFT);
-
     Rectangle backgroundRect = new Rectangle();
     backgroundRect.setFill(Color.BLACK);
     backgroundRect.setWidth(640);
@@ -45,11 +44,12 @@ public class GameSelection extends StackPane {
     final FadeTransition fadeInSelection = new FadeTransition(Duration.millis(500));
     final FadeTransition fadeOutBackground = new FadeTransition(Duration.millis(1000));
 
-    final GridPane outerPane = new GridPane();
+    final VBox outerPane = new VBox();
+    outerPane.setAlignment(Pos.CENTER);
     outerPane.setVisible(false);
     outerPane.setOpacity(0.0);
     outerPane.setPadding(new Insets(0, 15, 0, 15));
-    outerPane.setVgap(10);
+    outerPane.setSpacing(20);
 
     final ImageView background = new ImageView();
     background.setCursor(Cursor.HAND);
@@ -79,8 +79,6 @@ public class GameSelection extends StackPane {
     Label numberOfPlayersLabel = new Label("NUMBER OF PLAYERS");
     numberOfPlayersLabel.setFont(Assets.getFonts().get(Assets.FONT_COURIERNEW_BOLD_16));
     numberOfPlayersLabel.setTextFill(Color.LIGHTGRAY);
-    GridPane.setConstraints(numberOfPlayersLabel, 0, 0);
-    GridPane.setColumnSpan(numberOfPlayersLabel, 5);
 
     numberOfPlayers = new ToggleGroup();
 
@@ -89,7 +87,6 @@ public class GameSelection extends StackPane {
     numberOfPlayers1.setSelected(true);
     numberOfPlayers1.setUserData(1);
     numberOfPlayers1.setToggleGroup(numberOfPlayers);
-    GridPane.setConstraints(numberOfPlayers1, 0, 1);
     numberOfPlayers1.setMinWidth(120);
     numberOfPlayers1.setMaxWidth(120);
 
@@ -97,7 +94,6 @@ public class GameSelection extends StackPane {
     numberOfPlayers2.setFont(Assets.getFonts().get(Assets.FONT_COURIERNEW_BOLD_13));
     numberOfPlayers2.setUserData(2);
     numberOfPlayers2.setToggleGroup(numberOfPlayers);
-    GridPane.setConstraints(numberOfPlayers2, 1, 1);
     numberOfPlayers2.setMinWidth(120);
     numberOfPlayers2.setMaxWidth(120);
 
@@ -105,24 +101,20 @@ public class GameSelection extends StackPane {
     numberOfPlayers3.setFont(Assets.getFonts().get(Assets.FONT_COURIERNEW_BOLD_13));
     numberOfPlayers3.setUserData(3);
     numberOfPlayers3.setToggleGroup(numberOfPlayers);
-    GridPane.setConstraints(numberOfPlayers3, 2, 1);
-    numberOfPlayers3.setMinWidth(120);
-    numberOfPlayers3.setMaxWidth(120);
+
+    HBox numberOfPlayersButtons = new HBox();
+    numberOfPlayersButtons.setAlignment(Pos.CENTER);
+    numberOfPlayersButtons.getChildren().addAll(numberOfPlayers1,
+            numberOfPlayers2, numberOfPlayers3);
 
     Line horizontalLine1 = new Line();
-    horizontalLine1.setTranslateX(30);
-    horizontalLine1.setEndX(550);
+    horizontalLine1.setEndX(480);
     horizontalLine1.setStroke(Color.GRAY);
-    horizontalLine1.setStrokeWidth(3);
-    GridPane.setConstraints(horizontalLine1, 0, 2);
-    GridPane.setColumnSpan(horizontalLine1, 5);
-    GridPane.setMargin(horizontalLine1, new Insets(15, 0, 15, 0));
+    horizontalLine1.setStrokeWidth(2);
 
     Label worldCreationLabel = new Label("CREATION OF THE WORLD");
     worldCreationLabel.setFont(Assets.getFonts().get(Assets.FONT_COURIERNEW_BOLD_16));
     worldCreationLabel.setTextFill(Color.LIGHTGRAY);
-    GridPane.setConstraints(worldCreationLabel, 0, 3);
-    GridPane.setColumnSpan(worldCreationLabel, 5);
 
     worldCreation = new ToggleGroup();
 
@@ -133,7 +125,6 @@ public class GameSelection extends StackPane {
     worldCreation1.setToggleGroup(worldCreation);
     worldCreation1.setMinWidth(120);
     worldCreation1.setMaxWidth(120);
-    GridPane.setConstraints(worldCreation1, 0, 4);
 
     RadioButton worldCreation2 = new RadioButton("2");
     worldCreation2.setFont(Assets.getFonts().get(Assets.FONT_COURIERNEW_BOLD_13));
@@ -141,7 +132,6 @@ public class GameSelection extends StackPane {
     worldCreation2.setToggleGroup(worldCreation);
     worldCreation2.setMinWidth(120);
     worldCreation2.setMaxWidth(120);
-    GridPane.setConstraints(worldCreation2, 1, 4);
 
     RadioButton worldCreation3 = new RadioButton("3");
     worldCreation3.setFont(Assets.getFonts().get(Assets.FONT_COURIERNEW_BOLD_13));
@@ -149,7 +139,6 @@ public class GameSelection extends StackPane {
     worldCreation3.setToggleGroup(worldCreation);
     worldCreation3.setMinWidth(120);
     worldCreation3.setMaxWidth(120);
-    GridPane.setConstraints(worldCreation3, 2, 4);
 
     RadioButton worldCreation4 = new RadioButton("4");
     worldCreation4.setFont(Assets.getFonts().get(Assets.FONT_COURIERNEW_BOLD_13));
@@ -157,30 +146,25 @@ public class GameSelection extends StackPane {
     worldCreation4.setToggleGroup(worldCreation);
     worldCreation4.setMinWidth(120);
     worldCreation4.setMaxWidth(120);
-    GridPane.setConstraints(worldCreation4, 3, 4);
 
     RadioButton worldCreation5 = new RadioButton("5");
     worldCreation5.setFont(Assets.getFonts().get(Assets.FONT_COURIERNEW_BOLD_13));
     worldCreation5.setUserData(5);
     worldCreation5.setToggleGroup(worldCreation);
-    worldCreation5.setMinWidth(120);
-    worldCreation5.setMaxWidth(120);
-    GridPane.setConstraints(worldCreation5, 4, 4);
 
+    HBox worldCreationButtons = new HBox();
+    worldCreationButtons.setAlignment(Pos.CENTER);
+    worldCreationButtons.getChildren().addAll(worldCreation1, worldCreation2,
+            worldCreation3, worldCreation4, worldCreation5);
+    
     Line horizontalLine2 = new Line();
-    horizontalLine2.setTranslateX(30);
-    horizontalLine2.setEndX(550);
+    horizontalLine2.setEndX(480);
     horizontalLine2.setStroke(Color.GRAY);
-    horizontalLine2.setStrokeWidth(3);
-    GridPane.setConstraints(horizontalLine2, 0, 5);
-    GridPane.setColumnSpan(horizontalLine2, 5);
-    GridPane.setMargin(horizontalLine2, new Insets(15, 0, 15, 0));
+    horizontalLine2.setStrokeWidth(2);
 
     Label gameLevelLabel = new Label("GAME LEVEL");
     gameLevelLabel.setFont(Assets.getFonts().get(Assets.FONT_COURIERNEW_BOLD_16));
     gameLevelLabel.setTextFill(Color.LIGHTGRAY);
-    GridPane.setConstraints(gameLevelLabel, 0, 6);
-    GridPane.setColumnSpan(gameLevelLabel, 5);
 
     gameLevel = new ToggleGroup();
 
@@ -190,7 +174,6 @@ public class GameSelection extends StackPane {
     gameLevel1.setToggleGroup(gameLevel);
     gameLevel1.setMinWidth(120);
     gameLevel1.setMaxWidth(120);
-    GridPane.setConstraints(gameLevel1, 0, 7);
 
     RadioButton gameLevel2 = new RadioButton("NORMAL");
     gameLevel2.setFont(Assets.getFonts().get(Assets.FONT_COURIERNEW_BOLD_13));
@@ -199,18 +182,19 @@ public class GameSelection extends StackPane {
     gameLevel2.setToggleGroup(gameLevel);
     gameLevel2.setMinWidth(120);
     gameLevel2.setMaxWidth(120);
-    GridPane.setConstraints(gameLevel2, 1, 7);
 
     RadioButton gameLevel3 = new RadioButton("ADVANCED");
     gameLevel3.setFont(Assets.getFonts().get(Assets.FONT_COURIERNEW_BOLD_13));
     gameLevel3.setUserData(Game.Difficulty.ADVANCED);
     gameLevel3.setToggleGroup(gameLevel);
-    gameLevel3.setMinWidth(120);
-    gameLevel3.setMaxWidth(120);
-    GridPane.setConstraints(gameLevel3, 2, 7);
 
+    HBox gameLevelButtons = new HBox();
+    gameLevelButtons.setAlignment(Pos.CENTER);
+    gameLevelButtons.getChildren().addAll(gameLevel1, gameLevel2, gameLevel3);
+    
     Button startNewGame = new Button("NEW GAME");
-    startNewGame.setFont(Assets.getFonts().get(Assets.FONT_COURIERNEW_BOLD_13));
+    VBox.setMargin(startNewGame, new Insets(25, 0, 0, 0));
+    startNewGame.setFont(Assets.getFonts().get(Assets.FONT_COURIERNEW_BOLD_16));
     startNewGame.setCursor(Cursor.HAND);
     startNewGame.setOnAction(new EventHandler<ActionEvent>() {
       @Override
@@ -240,16 +224,10 @@ public class GameSelection extends StackPane {
         genesia.setGame(game);
       }
     });
-    GridPane.setConstraints(startNewGame, 0, 8);
-    GridPane.setMargin(startNewGame, new Insets(25, 0, 0, 0));
 
-    outerPane.getChildren().addAll(numberOfPlayersLabel,
-            numberOfPlayers1, numberOfPlayers2, numberOfPlayers3,
-            horizontalLine1,
-            worldCreationLabel, worldCreation1, worldCreation2,
-            worldCreation3, worldCreation4, worldCreation5,
-            horizontalLine2,
-            gameLevelLabel, gameLevel1, gameLevel2, gameLevel3,
+    outerPane.getChildren().addAll(numberOfPlayersLabel, numberOfPlayersButtons,
+            horizontalLine1, worldCreationLabel, worldCreationButtons,
+            horizontalLine2, gameLevelLabel, gameLevelButtons,
             startNewGame);
 
     getChildren().addAll(backgroundRect, background, outerPane);
