cb4a2bf33c549a3930b7ca42f6dbba5383928d51
Skyora <Skyora@PC_van_Skyora>
2012-01-22 16:34:56 +0100

    generate nodes once

--- genesia/src/main/java/net/sertik/genesia/render/SimpleRenderer.java
+++ genesia/src/main/java/net/sertik/genesia/render/SimpleRenderer.java
@@ -31,20 +31,21 @@ public class SimpleRenderer implements Renderer {
   public void render(Group container, double width, double height) {
     if (resourceLoader == null) throw new RuntimeException("No ResourceLoader specified.");
 
-    List<Node> nodes = new LinkedList<Node>();
-    for (int i = 0; i < world.getSizeSqrt(); i++) {
-      for (int j = 0; j < world.getSizeSqrt(); j++) {
-        Tile tile = world.getTile(i, j);
-        for (GameObject object : tile.getObjects()) {
-          Node node = resourceLoader.createResource(object);
-          node.setLayoutX(container.getLayoutX() + World.TILE_WIDTH / 2 * (i - j));
-          node.setLayoutY(container.getLayoutY() + World.TILE_HEIGHT / 2 * (i + j));
-          nodes.add(node);
-        }
-      }
-    }
+		if (container.getChildren().isEmpty()) {
+			List<Node> nodes = new LinkedList<Node>();
+			for (int i = 0; i < world.getSizeSqrt(); i++) {
+				for (int j = 0; j < world.getSizeSqrt(); j++) {
+					Tile tile = world.getTile(i, j);
+					for (GameObject object : tile.getObjects()) {
+						Node node = resourceLoader.createResource(object);
+						node.setLayoutX(World.TILE_WIDTH / 2 * (i - j));
+						node.setLayoutY(World.TILE_HEIGHT / 2 * (i + j));
+						nodes.add(node);
+					}
+				}
+			}
 
-    container.getChildren().clear();
-    container.getChildren().addAll(nodes);
+			container.getChildren().addAll(nodes);
+		}
   }
 }
